3f1d94f524478fa163e6081065bea5cd416629f4
Ahmed re <momo3956248@gmail.com>
2024-09-21 03:19:44 +0300

    Update & realTimeSearch

--- JS/index.js
+++ JS/index.js
@@ -9,10 +9,16 @@ let messageValidation = document.getElementById('messageValidation');
 
 let closeHint = document.getElementById('closeHint');
 
+let submitData = document.getElementById('submitData');
+
+let buttonUpdateBookmark = document.getElementById('buttonUpdateBookmark');
+let searchInput = document.getElementById('searchInput');
+
 let nameRegex = /^[a-zA-Z _-]{3,20}$/;
 let urlRegex =
   /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)/;
 
+let updateBookmarkIndex;
 let allBookmark = JSON.parse(localStorage.getItem('allBookmark')) || [];
 displayAllBookmark();
 
@@ -54,6 +60,11 @@ function displayBookmark(index) {
       <i class="fa-solid fa-trash-can text-light fs-5" ></i>
       Delete</button>
     </td>
+    <td>
+      <button class=" btn btn-warning border-0 px-2 fs-6 text-nowrap" onclick="getBookmarkInfo(${index})">
+      <i class="fa-solid fa-pen-nib text-light fs-5" ></i>
+      Update</button>
+    </td>
   </tr>
   `;
   tableData.innerHTML += mark;
@@ -79,6 +90,40 @@ function validtion(regex, ele) {
   ele.classList.remove('is-valid');
   ele.classList.add('is-invalid');
   return false;
+}
+function getBookmarkInfo(getIndexInfo) {
+  updateBookmarkIndex = getIndexInfo;
+  nameInput.value = allBookmark[getIndexInfo].bookmarkName;
+  urlInput.value = allBookmark[getIndexInfo].websireUrl;
+  submitData.classList.add('d-none');
+  buttonUpdateBookmark.classList.remove('d-none');
+}
+function updateBookmark() {
+  if (validtion(nameRegex, nameInput) && validtion(urlRegex, urlInput)) {
+    allBookmark[updateBookmarkIndex].bookmarkName = nameInput.value;
+    allBookmark[updateBookmarkIndex].websireUrl = urlInput.value;
+    localStorage.setItem('allBookmark', JSON.stringify(allBookmark));
+    tableData.innerHTML = '';
+    displayAllBookmark();
+    clearInput();
+    submitData.classList.remove('d-none');
+    buttonUpdateBookmark.classList.add('d-none');
+    console.log(updateBookmarkIndex);
+  } else {
+    messageValidation.classList.remove('d-none');
+  }
+}
+function searchBookMark() {
+  tableData.innerHTML = '';
+  for (let i = 0; i < allBookmark.length; i++) {
+    if (
+      allBookmark[i].bookmarkName
+        .toLowerCase()
+        .includes(searchInput.value.toLowerCase())
+    ) {
+      displayBookmark(i);
+    }
+  }
 }
 
 function closeWindowHint() {
